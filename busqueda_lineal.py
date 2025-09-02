24616b979c38fc733ec9d8a0f70fbc0cb381ede4
Saul-CD <150992710+Saul-CD@users.noreply.github.com>
2025-09-02 12:47:10 -0600

    Formato

--- busqueda_lineal.py
+++ busqueda_lineal.py
@@ -1,6 +1,4 @@
-def busqueda_lineal(
-    arreglo: list[int], k: int
-):  # S(n) = 1 + 1 = 2, porque se pasan por referencia
+def busqueda_lineal(arreglo: list[int], k: int):  # S(n) = 1 + 1 = 2, porque se pasan por referencia
     comparaciones: int = 0  # T(n) = 1, S(n) = 1
 
     for elemento in arreglo:  # T(n) = n, S(n) = 1
@@ -12,5 +10,6 @@ def busqueda_lineal(
 
     return comparaciones  # T(n) = 1, S(n) = 0
 
+    # ECUACIONES T(n) y S(n)
     # T(n)= 1 + 4n + 1 = 4n + 2
     # S(n)= 2 + 1 + 1 + 1 = 5
